45ba6d609e6b75602f85b5ee3cfdd34af2c0c8eb
lucastafer <lucastafer.dev@gmail.com>
2022-07-10 03:10:18 -0300

    api update

---
+++ packages/api/config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+
+require('dotenv').config();
+const MONGO_URL = process.env.MONGO_URL;
+mongoose
+  .connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("MongoDB connected succesfull."))
+  .catch((err) => console.log(err));
